7ed36f77c17ea7ceeb0b3e66fc8305e26bed3e96
Paahn <paahndev@gmail.com>
2021-03-24 21:49:41 -0700

    cleaned up codebase
    deleted console logs and comments

--- active-navbar.js
+++ active-navbar.js
@@ -1,5 +1,4 @@
 function selectElementByClass(className) {
-    console.log("logging the class....", document.querySelector(`.${className}`))
     return document.querySelector(`.${className}`);
 }
 
@@ -36,16 +35,9 @@ const observerOptions = {
   
 function observerCallback(entries, observer) {
   entries.forEach((entry) => {
-      console.log(`entry is ${JSON.stringify(entry)}`)
     if (entry.isIntersecting) {
-      // get the nav item corresponding to the id of the section
-      // that is currently in view
       const navItem = navItems[entry.target.id];
-      console.log(`navItem is ${navItem}`)
-      // add 'active' class on the navItem
       navItem.classList.add('active');
-      // remove 'active' class from any navItem that is not
-      // same as 'navItem' defined above
       Object.values(navItems).forEach((item) => {
         if (item != navItem) {
           item.classList.remove('active');
