8be9abf2f307ddf6f55781f606f2856c73cf1234
Paahn <paahndev@gmail.com>
2021-04-13 14:08:12 -0700

    remove extension from images when resizing and containing image

--- resize-images.js
+++ resize-images.js
@@ -4,6 +4,8 @@ const directory = './assets';
 
 fs.readdirSync(directory).forEach(file => {
   sharp(`${directory}/${file}`)
-    .resize(200, 100) // width, height
-    .toFile(`${directory}/${file}-small.jpg`);
+    .resize(200, 100, {
+      fit: 'contain'
+    }) // width, height, object fit
+    .toFile(`${directory}/${file.replace('.png', '')}-small.jpg`);
   });
